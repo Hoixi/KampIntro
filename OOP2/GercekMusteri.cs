b8e4326b5ed05aea53e569ae34185bb86e68675e
Furkan Kayali <furkankayali0h@gmail.com>
2023-11-13 08:48:46 +0300

    Yazılım Geliştirici Yetiştirme Kampı 5. Gün C#

---
+++ OOP2/GercekMusteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    class GercekMusteri:Musteri
+    {
+        public string Adi { get; set; } = string.Empty;
+        public string Soyadi { get; set; } = string.Empty;
+        public string TcNo { get; set; } = string.Empty;
+    }
+}
---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager, List<ILoggerService> loggerService)
+        {
+            //Başvuran bilgilerini değerlendirme
+            //
+            krediManager.Hesapla();
+            foreach (var logger in loggerService)
+            {
+                logger.Log();
+            }
+            
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
